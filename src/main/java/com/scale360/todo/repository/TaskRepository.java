f1f5cd5e9a737e5dce772c6911914dfaf7f9942a
Sirimongkol Panwa <sirimongkol.pan@ascendcorp.com>
2017-04-28 17:29:10 +0700

    Change CrudRepo to JpaRepo bc use findAll

--- src/main/java/com/scale360/todo/repository/TaskRepository.java
+++ src/main/java/com/scale360/todo/repository/TaskRepository.java
@@ -1,9 +1,9 @@
 package com.scale360.todo.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.scale360.todo.domain.Task;
 
-public interface TaskRepository extends CrudRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }
