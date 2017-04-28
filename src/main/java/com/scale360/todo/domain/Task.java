63d9c4ba8d6efae4ddf5258b8135c9709bca9d4c
Sirimongkol Panwa <sirimongkol.pan@ascendcorp.com>
2017-04-28 16:45:42 +0700

    Create Task Model.

---
+++ src/main/java/com/scale360/todo/domain/Task.java
@@ -0,0 +1,32 @@
+package com.scale360.todo.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Task")
+@Getter
+@Setter
+public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "subject")
+	private String subject;
+	
+	@Column(name = "detail", nullable = true)
+	private String detail;
+	
+	@Column(name = "status")
+	private String status = "pending";
+}
